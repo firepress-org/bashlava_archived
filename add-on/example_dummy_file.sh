e48ebd5bc9ff7a6f2f6bba581119cb8feb6ac10c
Pascal Andy <pascalandy@users.noreply.github.com>
2022-04-22 16:33:55 -0400

    New feat: pr

--- add-on/example_dummy_file.sh
+++ add-on/example_dummy_file.sh
@@ -3,4 +3,17 @@
 gh pr create \
   --fill \
   --assignee "@me" \
-  --base firepress-org/bashlava...master
+  --base firepress-org/bashlava
+
+
+###
+
+git remote -v
+
+  origin  git@github.com:firepress-org/bashlava.git (fetch)
+  origin  git@github.com:firepress-org/bashlava.git (push)
+  upstream        https://github.com/ralish/bash-script-template.git (fetch)
+  upstream        https://github.com/ralish/bash-script-template.git (push)
+
+git remote set-url upstream git@github.com:firepress-org/bashlava.git
+
