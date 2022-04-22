72d7e8d2fab837e8bc5ca7d50d65b97757ed9704
Pascal Andy <pascalandy@users.noreply.github.com>
2022-04-22 17:05:45 -0400

    New feature: pr (pull request without using the GUI

--- bashlava.sh
+++ bashlava.sh
@@ -92,10 +92,14 @@ function pr {
   App_Is_commit_unpushed
   App_Get_var_from_dockerfile
 
-  gh pr create \
-    --fill
-    --assignee "@me" \
-    --base "https://github.com/${github_user}/${app_name}/tree/${default_branch}" \
+  pr_title=$(git log --format=%B -n 1 $(git log -1 --pretty=format:"%h") | cat -)
+  gh pr create --fill --title "${pr_title}" --base "${default_branch}"
+
+  #pr_title=$(git log -n 2 | tail -n1 | awk '{print $2}')
+
+ # if the upstream is wrong, we can reset it:
+ # https://github.com/cli/cli/issues/2300
+ # git config --local --get-regexp '\.gh-resolved$' | cut -f1 -d' ' | xargs -L1 git config --unset
 
   echo "wip 0o0o 2022-04-22_16h00"
 }
