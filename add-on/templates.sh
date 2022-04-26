7e8b19b508298a76c91216b2afd4f43c288c6d2c
Pascal Andy <pascalandy@users.noreply.github.com>
2022-04-26 17:00:36 -0400

    add back to fct banner / dep from fct test
    Signed-off-by: Pascal Andy <pascalandy@users.noreply.github.com>

--- add-on/templates.sh
+++ add-on/templates.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+function banner {
+  figlet_message="Banner Test"
+  App_figlet
+}
+
 function init_changelog {
 cat << EOF > CHANGELOG_template.md
 This **CHANGELOG** should make it easy for you to see what is happening with this project. Each release automatically generates/formats/updates this changeling thanks to [BashLaVa](https://github.com/firepress-org/bashlava). It uses the same « Agile Deployment Lifecycle » we use at FirePress. See our [blog post](https://firepress.org/en/how-do-we-update-hundreds-of-ghosts-websites-on-docker-swarm/) about it. It follows the [keep a changelog](https://keepachangelog.com/en/1.0.0/) spirit and adheres to [semantic versioning](https://semver.org/spec/v2.0.0.html).
