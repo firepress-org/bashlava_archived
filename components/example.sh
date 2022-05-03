aa533f7a47b51b4366b43541d847a2881d68db67
Pascal Andy <pascalandy@users.noreply.github.com>
2022-05-03 17:18:19 -0400

    Refactor example case_c

--- components/example.sh
+++ components/example.sh
@@ -83,19 +83,11 @@ function case_b {
 
 # Example 3: for Loops
 function case_c {
-  for file in $(ls)
+
+  for f in *.wav
   do
-  Extension=${file##*.}
-  case "$Extension" in
-    sh) echo "Shell script: $file";;
-    md) echo "A markdown file: $file";;
-    png) echo "PNG image file: $file";;
-    txt) echo "A text file: $file";;
-    zip) echo "An archive: $file";;
-    conf) echo "A configuration file: $file";;
-    py) echo "A Python script: $file";;
-    *) echo "Unknown file type: $file";;
-  esac
+    [[ -e "$f" ]] || break  # handle the case of no *.wav files
+    echo "$f"
   done
 }
 
